ec8de1f8f59175ccaed813ac7e0dc0d51ba99fd7
Oscar Alshammar <65617991+j0rry@users.noreply.github.com>
2024-08-05 16:23:13 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -84,6 +84,7 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<C-b>", "<Cmd>Neotree toggle<CR>")
 vim.keymap.set("i", "<C-b>", "<Cmd>Neotree toggle<CR>")
+vim.api.nvim_set_keymap('n', '<C-b>', ':Neotree toggle<CR>', { noremap = true, silent = true })
 vim.keymap.set({ "i" }, "<C-s>", "<C-o>:w<ENTER>")
 vim.keymap.set({ "n" }, "<C-s>", ":w<ENTER>")
 
